b0bc25e3b670b6655914f56f92e7c6eff0853593
UIUCFriendProject <uiucfriendproject@gmail.com>
2025-12-07 21:11:37 -0600

    Fix compilation

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,6 @@ import { createApp } from 'vue'
 // Styles
 import 'unfonts.css'
 
-import { useAppStore } from '@/stores/app';
-
-const store = useAppStore();
-
-await store.initAuth();
 
 const app = createApp(App)
 
