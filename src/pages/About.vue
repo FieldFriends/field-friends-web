df3b49ac7327d3e9c6a8debbd04db020a38c9bd5
FieldFriends <fieldfriends.team@gmail.com>
2026-02-19 21:06:49 -0600

    Add temporary About and FAQ pages

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,26 @@
 <template>
-  About
+  <v-container class="fill-height d-flex flex-column justify-center align-center text-center">
+    <h1 class="text-h2 font-weight-bold mb-6 text-primary">
+      About Us
+    </h1>
+
+    <v-icon
+      icon="mdi-hammer-wrench"
+      size="96"
+      color="secondary"
+      class="mb-8 opacity-50"
+    />
+
+    <p class="text-h5 text-secondary font-dm-sans font-weight-regular mb-12">
+      Under construction. Check back soon!
+    </p>
+
+    <v-btn
+      size="large"
+      to="/"
+      prepend-icon="mdi-home"
+    >
+      Go Back Home
+    </v-btn>
+  </v-container>
 </template>
