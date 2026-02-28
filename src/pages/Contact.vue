5407f436ad6c1162e3007b2e61e2977f02ed3db7
FieldFriends <fieldfriends.team@gmail.com>
2026-02-27 22:37:34 -0600

    Update contact information

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -11,21 +11,36 @@
         <div>
           <p class="text-body-1 text-secondary page-text">
             For questions, feedback, or assistance, please reach out to:
-            <a href="mailto:fieldfriends.team@gmail.com" class="text-primary font-weight-bold text-decoration-none">fieldfriends.team@gmail.com</a>
+            <a
+              href="mailto:fieldfriends.team@gmail.com"
+              class="text-primary font-weight-bold text-decoration-none"
+            >
+              fieldfriends.team@gmail.com
+            </a>
           </p>
         </div>
 
         <div>
           <p class="text-body-1 text-secondary page-text">
             Automated messages, such as login codes and match introductions, will be sent from:
-            <a href="mailto:noreply@fieldfriends.org" class="text-primary font-weight-bold text-decoration-none" tabindex="-1" style="pointer-events: none;">noreply@fieldfriends.org</a>
+            <strong>noreply@fieldfriends.org</strong>
+          </p>
+        </div>
+
+        <div>
+          <p class="text-body-1 text-secondary page-text">
+            Our official Reddit account is <strong>u/FieldFriendsTeam</strong>.
           </p>
         </div>
       </div>
 
+
       <div class="mt-8">
         <p class="text-body-1 text-secondary page-text">
           We will never contact you from any other email addresses. Additionally, because Field Friends operates in distinct monthly rounds, you will only receive automated emails during a round you have actively signed up for.
+        </p>
+        <p class="text-body-1 text-secondary page-text mt-4">
+          We don't currently have any other social media accounts.
         </p>
       </div>
 
