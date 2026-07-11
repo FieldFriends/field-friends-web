bb0858c5d7fa8b8d94cd215d836cbf2b5c7a0fc8
FieldFriends <fieldfriends.team@gmail.com>
2026-07-10 23:10:34 -0500

    Remove boldness from date

--- src/components/MatchingInfo.vue
+++ src/components/MatchingInfo.vue
@@ -15,7 +15,7 @@
           Sign-ups will close on <span class="font-weight-bold">{{ getWindowEndDateString }}</span>.
         </p>
         <p class="text-body-1 text-secondary mt-2">
-          Matches will be sent out on <span class="font-weight-bold">{{ getMatchDateString }}</span>.
+          Matches will be sent out on {{ getMatchDateString }}.
         </p>
       </template>
 
